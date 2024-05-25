dec2612472e26bd55bd3d43441b0b2316a0e3866
Dinesh Kundo <60452355+Dineshkundo@users.noreply.github.com>
2024-05-25 14:22:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_storage_bucket" "my-bucket" {
 #
 resource "google_storage_bucket" "my-bucket2" {
   name                     = "tt-githubdemo-bucket-002"
-  project                  = "tt-dev-001"
+  project                  = "lasya-ganta-14"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
